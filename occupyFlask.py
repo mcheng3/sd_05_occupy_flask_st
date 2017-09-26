0dd69ce8834eb7f7d02b7a0e8b38dc8df882f11f
mcheng3 <michael.cheng65@gmail.com>
2017-09-26 14:46:58 -0400

    Update occupyFlask.py

--- occupyFlask.py
+++ occupyFlask.py
@@ -1,3 +1,15 @@
 import occupation
+from flask import Flask, render_template
 
 print(occupation.pickOccupation())
+
+my_app = Flask(__name__)
+
+@my_app.route('/occupations')
+def displayOccupation():
+    
+    return render_template('template.html', job = occupation.pickOccupation())
+
+if __name__ == '__main__':
+        my_app.debug = True
+        my_app.run()
